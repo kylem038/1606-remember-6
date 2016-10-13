7321a8b6833a05e94399a8f1d5932dca86165280
kylem038 <misenckj@gmail.com>
2016-10-13 14:39:16 -0600

    fixed date bug & all tests pass

--- app/routes/reminders/new.js
+++ app/routes/reminders/new.js
@@ -8,6 +8,12 @@ export default Ember.Route.extend({
 
   actions: {
     createReminder(reminderForm) {
+      let stringDate = reminderForm.get('date');
+      if(stringDate) {
+        reminderForm.set('date', new Date(stringDate));
+      } else {
+        reminderForm.set('date', new Date());        
+      }
       let reminderProps = reminderForm.getProperties('title', 'date', 'notes');
       let record = this.get('store').createRecord('reminder', reminderProps);
       record.save().then(() => {
