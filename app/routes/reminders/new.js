e016154e13046804096d6689c559e3082fb9d592
kylem038 <misenckj@gmail.com>
2016-10-13 14:17:53 -0600

    Can now add a reminder to the reminders

--- app/routes/reminders/new.js
+++ app/routes/reminders/new.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model() {
+    return Ember.Object.create();
+  },
+
+  actions: {
+    createReminder(reminderForm) {
+      let reminderProps = reminderForm.getProperties('title', 'date', 'notes');
+      let record = this.get('store').createRecord('reminder', reminderProps);
+      record.save().then(() => {
+        return this.transitionTo('reminders.new');
+      });
+    }
+  }
 });
--- tests/acceptance/reminder-list-test.js
+++ tests/acceptance/reminder-list-test.js
@@ -34,3 +34,19 @@ test('clicking on an individual item', function(assert) {
     assert.equal(Ember.$('.spec-reminder-item:first').text().trim(), Ember.$('.spec-reminder-title').text().trim());
   });
 });
+
+test('adds an individual reminder on form submit', function(assert) {
+
+  visit('/');
+  click('.spec-add-new-reminder');
+  fillIn('.spec-new-title-input', 'Tom Cruise');
+  fillIn('.spec-new-date-input', '12/11/2017');
+  fillIn('.spec-new-note-input', 'Star in every movie ever');
+  click('.spec-submit-reminder');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/new');
+    assert.equal(find('.spec-reminder-item').length, 6, 'should show one additional reminder');
+    assert.equal(find('.spec-reminder-item:last').text().trim(), 'Tom Cruise', 'should show new reminder title');
+  });
+});
