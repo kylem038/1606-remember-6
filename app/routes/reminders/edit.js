1b5df1665d0f99be815d22a7e87f3f5368aff5a4
Adam Bernard <ab255@me.com>
2016-10-17 17:59:09 -0600

    update findRecord and editReminder function

--- app/routes/reminders/edit.js
+++ app/routes/reminders/edit.js
@@ -3,18 +3,18 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model(params) {
-    return this.get('store').find('reminder', params.reminder_id);
+    return this.get('store').findRecord('reminder', params.reminder_id);
   },
 
   actions: {
-    editReminder(reminderForm) {
-      let stringDate = reminderForm.get('date');
-      reminderForm.set('title', reminderForm.get('title'));
+    editReminder(editReminderForm) {
+      let stringDate = editReminderForm.get('date');
+      editReminderForm.set('title', editReminderForm.get('title'));
       if(stringDate) {
-        reminderForm.set('date', new Date(stringDate));
+        editReminderForm.set('date', new Date(stringDate));
       }
-      reminderForm.set('notes', reminderForm.get('notes'));
-      reminderForm.save().then((reminder) => {
+      editReminderForm.set('notes', editReminderForm.get('notes'));
+      editReminderForm.save().then((reminder) => {
         this.transitionTo('reminders.reminder', reminder)
       });
     }
