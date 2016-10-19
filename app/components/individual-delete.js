27926084566137c5a1d612834df3c89efd2c526e
kylem038 <misenckj@gmail.com>
2016-10-19 08:05:20 -0600

    Cleaned up files - delete now functional

--- app/components/individual-delete.js
+++ app/components/individual-delete.js
@@ -1,10 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  
+  store: Ember.inject.service(),
+  routing: Ember.inject.service('-routing'),
+
   actions: {
     destroyIndividualReminder(reminder) {
       reminder.destroyRecord();
-        // this.transitionToRoute('reminders');
+        this.get('routing').transitionTo('reminders');
     }
   }
 });
