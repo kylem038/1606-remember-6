ed2f4bf862621aa706c027088d3a86001e65188f
kylem038 <misenckj@gmail.com>
2016-10-19 08:11:57 -0600

    Refactored into one button

--- app/components/reusable-button.js
+++ app/components/reusable-button.js
@@ -1,9 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
+  routing: Ember.inject.service('-routing'),
+
   actions: {
-    destroyReminder(reminder) {
+    destroyIndividualReminder(reminder) {
       reminder.destroyRecord();
+        this.get('routing').transitionTo('reminders');
     }
   }
 });
